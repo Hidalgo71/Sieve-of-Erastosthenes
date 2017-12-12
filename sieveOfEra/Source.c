1dc7ec7990e6bd49c96d6768719f3848cb6bb3f5
Yekta Özdemir <y.ozdemir71@gmail.com>
2017-12-12 20:57:49 +0300

    Updated.

--- sieveOfEra/Source.c
+++ sieveOfEra/Source.c
@@ -7,29 +7,76 @@ void sieveOfEra()
 	int checkArray[4] = { 2,3,5,7 };
 	int pI = 2, memAdd = 0, toFalse = 0, j = 0;
 
-	for ( ; pI < 151; pI++)
+	for (; pI < 151; pI++)
 	{
 		sieveArray[memAdd] = pI;
 		memAdd++;
 	}
-	for (; j < 5 ; j++)
+	for (; toFalse < 149; toFalse++)
 	{
-		for (; toFalse < 149; toFalse++)
+		if (sieveArray[toFalse] % checkArray[0] == 0)
 		{
-			if (sieveArray[toFalse] % checkArray[j] == 0)
+			if (sieveArray[toFalse] == 2)
 			{
-				sieveArray[toFalse] = 0;
+				continue;
 			}
+			sieveArray[toFalse] = 0;
 		}
 	}
-	
 
+	for (toFalse = 0; toFalse < 149; toFalse++)
+	{
+		if (sieveArray[toFalse] % checkArray[1] == 0)
+		{
+			if (sieveArray[toFalse] == 3)
+			{
+				continue;
+			}
+			sieveArray[toFalse] = 0;
+		}
+	}
+
+	for (toFalse = 0; toFalse < 149; toFalse++)
+	{
+		if (sieveArray[toFalse] % checkArray[2] == 0)
+		{
+			if (sieveArray[toFalse] == 5)
+			{
+				continue;
+			}
+			sieveArray[toFalse] = 0;
+		}
+	}
+
+	for (toFalse = 0; toFalse < 149; toFalse++)
+	{
+		if (sieveArray[toFalse] % checkArray[3] == 0)
+		{
+			if (sieveArray[toFalse] == 7)
+			{
+				continue;
+			}
+			sieveArray[toFalse] = 0;
+		}
+	}
+
+
+	//for (j = 0; j < SIZE; j++)
+	//{
+	//	for (i = 0; i < SIZE; i++)
+	//	{
+	//		scanf_s("%d", &A[i][j]);
+	//	}
+	//}
 
 	for (int j = 0; j < 149; j++)
 	{
-		printf("%d\n", sieveArray[j]);
+		if (sieveArray[j] != 0)
+		{
+			printf("%d\n", sieveArray[j]);
+		}
 	}
-	
+
 }
 
 int main()
