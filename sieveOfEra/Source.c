c03cab7f6305b5da4549e5c13b0a6716f9af222d
Yekta Özdemir <y.ozdemir71@gmail.com>
2017-12-14 20:59:36 +0300

    Its not a true solution. For now...

--- sieveOfEra/Source.c
+++ sieveOfEra/Source.c
@@ -4,7 +4,7 @@
 void sieveOfEra()
 {
 	int sieveArray[149] = { 0 };
-	int checkArray[4] = { 2,3,5,7 };
+	int checkArray[5] = { 2,3,5,7,11 };
 	int pI = 2, memAdd = 0, toFalse = 0, j = 0;
 
 	for (; pI < 151; pI++)
@@ -60,14 +60,17 @@ void sieveOfEra()
 		}
 	}
 
-
-	//for (j = 0; j < SIZE; j++)
-	//{
-	//	for (i = 0; i < SIZE; i++)
-	//	{
-	//		scanf_s("%d", &A[i][j]);
-	//	}
-	//}
+	for (toFalse = 0; toFalse < 149; toFalse++)
+	{
+		if (sieveArray[toFalse] % checkArray[4] == 0)
+		{
+			if (sieveArray[toFalse] == 11)
+			{
+				continue;
+			}
+			sieveArray[toFalse] = 0;
+		}
+	}
 
 	for (int j = 0; j < 149; j++)
 	{
@@ -85,46 +88,4 @@ int main()
 
 	system("PAUSE");
 	return 0;
-}
-
-//#include <stdio.h>
-//#include<math.h>
-//
-//void sieveOfEra()
-//{
-//	int sieveArray[149] = { 0 };
-//	int checkArray[4] = { 2,3,5,7 };
-//	int pI = 2, memAdd = 0, toFalse = 0, j = 0;
-//
-//	for (; pI < 151; pI++)
-//	{
-//		sieveArray[memAdd] = pI;
-//		memAdd++;
-//	}
-//	for (; j < 5; j++)
-//	{
-//		for (; toFalse < 149; toFalse++)
-//		{
-//			if (sieveArray[toFalse] % checkArray[j] == 0)
-//			{
-//				sieveArray[toFalse] = 0;
-//			}
-//		}
-//	}
-//
-//
-//
-//	for (int j = 0; j < 149; j++)
-//	{
-//		printf("%d\n", sieveArray[j]);
-//	}
-//
-//}
-//
-//int main()
-//{
-//	sieveOfEra();
-//
-//	system("PAUSE");
-//	return 0;
 }
